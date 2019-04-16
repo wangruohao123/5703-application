a3233197543a75e23e108273adec059d44d0b056
Boning He <48296783+BoningHe@users.noreply.github.com>
2019-04-17 06:46:33 +1000

    Add files via upload

---
+++ KINDER FOOD FINDER_09的副本/KINDER FOOD FINDER/Search Function/DetailMapCell.swift
@@ -0,0 +1,22 @@
+//
+//  DetailMapCell.swift
+//  KINDER FOOD FINDER
+//
+//  Created by heboning on 2019/04/10.
+//  Copyright © 2019 KINDER FOOD FINDER. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class DetailMapCell: UITableViewCell {
+
+    @IBOutlet var mapView: MKMapView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    
+}
